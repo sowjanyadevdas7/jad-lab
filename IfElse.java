679c65996a5fe11c9bc4804a75171b2301df0ce0
sowjanyadevdas7 <35593759+sowjanyadevdas7@users.noreply.github.com>
2018-09-07 16:10:12 +0530

    Add files via upload

---
+++ IfElse.java
@@ -0,0 +1,16 @@
+class IfElse 
+{
+ public static void main(String[] args) 
+ {    	
+  int number = 10;
+  if (number > 0) 
+  {
+   System.out.println("Number is positive.");
+  }
+  else 
+  {
+   System.out.println("Number is not positive.");
+  }
+  System.out.println("This statement is always executed.");
+ }
+}
